54e372781fa1da02a8c6537739505f7d1457552f
unknown <you@example.com>
2025-09-15 15:56:34 +0530

    array1

---
+++ array.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+int main()
+{
+    // int arr [5]={1,1,3,4,5};
+    // for(int i=0;i<5;i++)
+    // {
+        // printf("%d",arr[i]);
+    
+
+    
+        // printf("%d",arr[4]);
+    // }
+
+
+
+
+
+// int sum=0;
+// for(int i=0;i<5;i++)
+// {
+//     sum= sum+arr[i];
+//     printf("%d\n",sum);
+
+// int sum=0;
+// for (int i=0;i<3;i++)
+// sum =sum+arr[i];
+// printf("%d\n",sum);
+
+// int arr[5]={3,12,52,34,5};
+// int max =arr[0];
+// for (int i=1;i<5;i++){
+//     if(arr[i]>max){
+//         max=arr[i];
+//     }
+
+// }
+// /
+// / printf("%d\n",max);
+
+// int max =arr[0];
+// for(int i=0;i<5;i++)
+// {
+//     if (arr[i]>max)
+//     {
+//         max=are[i];
+//     }
+
+// }
+// printf("%d\n",max)
+// }
+
+
+
+// int i;
+// for(i=0;i<5;i++)
+// {
+//     if (arr[i]==5)
+//     {
+        
+//    printf("%d\n",arr[i]);
+//     }
+// }
+
+// int i;
+// for (int i=0;i<5;i++)
+// {
+//     if (arr[i]%2==0)
+//     {
+//         printf("%d\n",arr[i]);
+//     }
+//     else{
+//         printf("%d",arr[i]);
+//     }
+// }
+
+int arr[5]={3,12,52,34,5};
+int target=3;
+for(inti=0;i<5;i++){
+    if(arr[i]==target){
+        printf("%d",arr[i]);
+        break;
+    }
+}
+
+}
