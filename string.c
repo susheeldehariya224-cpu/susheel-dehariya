20538cf4b6497dd4bf22a8df6a643fd2bdf1da1f
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-10-14 15:15:44 +0530

    string

---
+++ string.c
@@ -0,0 +1,241 @@
+#include <stdio.h>
+#include<string.h>
+int main(){
+//     char str[]="abca";
+//     int freq[256]={0};
+//     int ln= strlen(str);
+//     for (int i=0;i<ln;i++)
+//     {
+//         freq[str[i]]++;
+
+//     }
+
+//     for (int i=0;i<256;i++){
+//         if (freq[i]>0){
+//         printf("%c:%d",i,freq[i]);
+
+//     }
+// }
+// }
+
+// char str[]="cabbabk";
+//     int freq[256]={0};
+//     int ln= strlen(str);
+//     for (int i=0;i<ln;i++)
+//     {
+//         freq[str[i]]++;
+
+//     }
+
+//     for (int i=0;i<256;i++){
+//         if (freq[i]>0){
+//         printf("%c:%d",i,freq[i]);
+
+//     }
+// }
+// }
+
+// char str[]="aaabbbcccddd";
+//     int freq[256]={0};
+//     int ln= strlen(str);
+//     for (int i=0;i<ln;i++)
+//     {
+//         freq[str[i]]++;
+
+//     }
+
+//     for (int i=0;i<256;i++){
+//         if (freq[i]>0){
+//         printf("%c:%d",i,freq[i]);
+
+//     }
+// }
+// }
+
+
+//     char str[]="skdsdsd";
+//     int freq[256]=0;
+//     int ln=strlen(str)
+
+
+//     for (int i=0;i<ln;i++);
+// {
+//     freq [str[i]]++;
+
+// }
+// for (int i=0;i<256;i++)
+// {
+//     if (freq[i]>0){
+//         printf("%c\n,%d\n:",i,freq[i]);
+
+//     }
+// }
+
+
+// char str[]="abcabcbcb";
+
+//     int freq[256]={0};
+    
+//     int ln= strlen(str);
+    
+//     for (int i=0;i<ln;i++)
+    
+//     {
+
+//         freq[str[i]]++;
+
+
+//     }
+
+//     for (int i=0;i<256;i++){
+
+
+//         if (freq[i]>0){
+        
+//             printf("%c:%d",i,freq[i]);
+
+//     }
+// }
+// }
+
+
+
+
+// char str[]="abcabcccbbbb";
+
+//     int freq [256]=0;
+//     char result;
+//     int max0;
+//     for (int i=o;i<ln;i++);
+// {
+//         freq [str[i]]++;
+
+//     }
+//     for (int i=0;i<ln;i++){
+//         if (freq[str[i]]>max){
+//             max=freq[str[i]];
+//             result=str[i];
+//         }
+//     }
+//    printf("%c ,-d ",i,freq[i])
+// }
+
+
+
+// char str []="abcabbbbb";
+// char substr[]="abc";
+
+//  if (strstr(str,substr)= !NULL)
+//  {
+//     printf("true");
+
+// }else{
+//     printf("false");
+
+// }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
