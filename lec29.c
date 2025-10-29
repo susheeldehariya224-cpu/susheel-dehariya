b4536314eba82690849b16f2de2a343ea7256fba
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-10-29 16:08:02 +0530

    Shift

---
+++ lec29.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+int main(){
+
+
+//     int arr[5]={4,5,8,4,5};
+    
+//     int xor=0;
+//     for (int i=0;i<5;i++){
+//         xor=xor^arr[i];
+//     }
+//     printf("%d",xor);
+
+// }
+
+
+// int arr[9]={2,4,5,8,7,5,2,4,8};
+// int xor=0;
+
+//     for (int i=0;i<9;i++){
+//         xor=xor^arr[i];
+//     }
+//     printf("%d",xor);
+// }
+
+
+// int a=5;
+// int b=3;
+// a=a^b;
+// b=a^b;
+// a=a^b;
+// printf("%d,%d",a,b);
+// }
+
+
+
+
+
+// int arr[4]={1,2,4,5};
+// int xor1=0;
+// int xor2=0;
+// for (int i=1;i<=5;i++){
+//     xor1=xor1^i;
+// }
+// for (int i=0;i<4;i++){
+//     xor2=xor2^arr[i];
+  
+
+// }
+
+
+// printf("%d",xor1^xor2);
+
+// int a=20;
+// int res= a>>2;
+// printf("%d",res);
+
+
+
+
+// int a=20;
+// int res= a<<4;
+// printf("%d",res);
+
+// }
+
+
+
+
+int a=20;
+int res= a<<6;
+printf("%d",res);
+
+}
+
+
+
