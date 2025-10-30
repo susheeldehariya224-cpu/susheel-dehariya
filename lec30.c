8d59143e9aec662b39d71db90b98c5b54af3914c
susheeldehariya224-cpu <susheeldehariya224@gmail.com>
2025-10-30 16:00:44 +0530

    RCSN

---
+++ lec30.c
@@ -0,0 +1,161 @@
+#include<stdio.h>
+// void sum(int a){
+//     if (a==0){
+//         return ;
+        
+//     }
+//     printf("%d",a);
+//     sum(a-1);
+// }
+
+// int main(){
+//     sum (3);
+// }
+
+
+
+
+
+// void sum(int a){
+//     if (a==0){
+//         return ;
+        
+//     }
+//         sum(a-1);
+//     printf("%d",a);
+  
+// }
+
+// int main(){
+//     sum (3);
+// }
+
+
+
+
+// void sum(int a){
+//     if (a==0){
+//         return ;
+        
+//     }
+//     printf("%d",a);
+//     sum(a-1);
+// }
+
+// int main(){
+//     sum (3);
+// }
+
+
+
+
+//  int sum(int a){
+//     if (a==0){
+//         return 0;
+//     }
+//         return a+sum(a-1) ;
+        
+//     }
+  
+//    int main()
+// {
+// int data =sum(3);
+// printf("%d",data);
+
+ 
+// }
+
+
+
+
+//  int fact(int a){
+//     if (a==0 ||a==1){
+//         return 1;
+//     }
+//         return a*fact(a-1) ;
+        
+//     }
+  
+//    int main()
+// {
+// int data =fact(3);
+// printf("%d",data);
+
+ 
+// }
+
+
+
+// int fact(int a){
+//     if (a==0 ||a==1){
+//         return 1;
+//     }
+//         return a*fact(a-1) ;
+        
+//     }
+  
+//    int main()
+// {
+// int data =fact(5);
+// printf("%d",data);
+
+ 
+// }
+
+
+
+
+// int sum (int arr[],int n){
+//     if (n==0){
+//         return 0;
+
+//     }
+//     return arr[n-1]+sum(arr,n-1);
+
+// }
+
+// int main()
+// {
+//     int arr[3]={3,4,1};
+//     int n=3;
+//     int data=sum(arr,n);
+//     printf("%d",data);
+
+// }
+
+
+
+
+// int sum (int arr[],int n){
+//     if (n==0){
+//         return 0;
+
+//     }
+//     return arr[n-1]+sum(arr,n-1);
+
+// }
+
+// int main()
+// {
+//     int arr[5]={3,4,1,5,6};
+//     int n=6;
+//     int data=sum(arr,n);
+//     printf("%d",data);
+    
+// }
+
+
+
+
+// int power(int a){
+//     if (a==0){
+//         return 1;
+//     }
+
+//     return 2*power(a-1);
+
+// }
+// int main(){
+//     int data =power(5);
+//     printf("%d",data);
+// }
